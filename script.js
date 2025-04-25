34dcfd633899d78cd50a8ce802625d00363d73eb
athulsin <160761986+athulsin@users.noreply.github.com>
2025-04-25 22:58:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.item-checkbox').forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+        // You could add code here to save the state to localStorage
+        // or perform other actions when items are checked/unchecked
+    });
+});
